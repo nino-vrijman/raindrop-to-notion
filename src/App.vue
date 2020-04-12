e7e8e8600ffe9f5d92c4386ab845f22356a463b6
Nino Vrijman <ninovrijman@gmail.com>
2020-04-12 21:29:53 +0200

    feat: adds dropzone, show 'copy' effect on file hover, only accept html files, read filecontents

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,43 @@
 <template>
-  <div id="app"></div>
+  <div
+    id="app"
+    class="drop-zone"
+    @dragover.stop.prevent="onDragover"
+    @drop.stop.prevent="onDrop"
+  >
+    Drop a file anywhere in the window
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  methods: {
+    onDragover(event) {
+      event.dataTransfer.dropEffect = 'copy';
+    },
+    onDrop(event) {
+      // Source: https://www.html5rocks.com/en/tutorials/file/dndfiles/
+      const [file] = event.dataTransfer.files;
+
+      if (!file.type.match('text/html')) {
+        console.log('Expecting HTML file, returning...');
+        return;
+      }
+
+      const reader = new FileReader();
+      reader.onload = function(event) {
+        const fileContent = event.target.result;
+        console.log('Do we have filecontents?', !!fileContent);
+      };
+      reader.readAsText(file);
+    }
+  }
 };
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+* {
+  box-sizing: border-box;
+}
+</style>
