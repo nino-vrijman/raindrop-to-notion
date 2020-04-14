8e1d2f788405e97d4b9551301c9f55c2b9ceac5a
Nino Vrijman <ninovrijman@gmail.com>
2020-04-14 22:04:07 +0200

    test(parse raindrop export): test undefined and object when respectively no string and a html string is supplied

--- tests/unit/parse-raindrop-export.spec.js
+++ tests/unit/parse-raindrop-export.spec.js
@@ -10,4 +10,12 @@ describe('#parseRaindropExport', () => {
   it('is a function', () => {
     expect(parseRaindropExport).to.be.a('function');
   });
+
+  it('should return undefined when no string was supplied', () => {
+    expect(parseRaindropExport()).to.be.undefined;
+  });
+
+  it('should return an object when a string was specified', () => {
+    expect(parseRaindropExport('<html></html>')).to.be.a('object');
+  });
 });
